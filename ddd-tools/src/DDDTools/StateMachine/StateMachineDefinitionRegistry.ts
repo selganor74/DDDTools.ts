c1008d474e3a1b48654e062cb444d889ce80ce01
Gabriele Rossetti <g.rossetti@loccioni.com>
2016-10-19 15:09:01 +0200

    * Added StateMachineDefinitionRegistry. Allows to "change" the stateMachineDefinitions, without the need of an upgrade process of the state machine. * Added the README.md as linked existing item in visual studio. Still don't know if it is the right choice as Visual Studio seems to store an absolut path to the file.

---
+++ ddd-tools/src/DDDTools/StateMachine/StateMachineDefinitionRegistry.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="BaseStateMachine.ts" />
+
+namespace DDDTools.StateMachine {
+
+    export class StateMachineDefinitionRegistry {
+
+        private static stateMachineDefinitionRegistry: { [typeAndVersion: string]: StateMachineDefinition<any, any> } = {};
+
+        private static buildTypeAndVersionString(typeName: string, typeVersion: string) {
+            return typeName + "-" + typeVersion;
+        }
+
+        public static registerStateMachine<TStatuses, TEvents>(
+            stateMachineType: string,
+            stateMachineVersion: string,
+            stateMachineDefinition: StateMachineDefinition<TStatuses, TEvents>
+        ): void {
+            var typeNameAndVersion = StateMachineDefinitionRegistry.buildTypeAndVersionString(stateMachineType, stateMachineVersion);
+            StateMachineDefinitionRegistry.stateMachineDefinitionRegistry[typeNameAndVersion] = stateMachineDefinition;
+        }
+
+        public static getStateMachine<TStatuses, TEvents>(stateMachineType: string, stateMachineVersion: string) {
+            var typeNameAndVersion = StateMachineDefinitionRegistry.buildTypeAndVersionString(stateMachineType, stateMachineVersion);
+            return StateMachineDefinitionRegistry.stateMachineDefinitionRegistry[typeNameAndVersion];
+        }
+    }
+}
